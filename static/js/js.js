07ffc6b882c4dd419d8b804f6380d63207e060f2
hsp86 <husipeng86@live.com>
2016-04-18 21:00:46 +0800

    添加上下键使用历史命令功能

--- static/js/js.js
+++ static/js/js.js
@@ -14,6 +14,13 @@ $(function(){
         pos = console_str.lastIndexOf(cmd_prompt);
         return console_str.substring(pos+cmd_prompt_len);
     }
+    // 将命令显示在命令窗口
+    function set_cmd($console,cur_cmd)
+    {
+        console_str = $console.val();
+        pos = console_str.lastIndexOf(cmd_prompt);
+        $console.val(console_str.substring(0,pos+cmd_prompt_len) + cur_cmd);
+    }
     // ajax返回命令执行并取得执行结果显示
     function execute_cmd(cmd_text)
     {
@@ -37,14 +44,60 @@ $(function(){
         }
     }
     $console.val('欢迎使用命令执行系统---胡祀鹏设计制作(2016.04.17)' + cmd_prompt);
+    history_cmd_before = [];
+    history_cmd_after = [];
+    cur_cmd = '';
+    up_down = false;
     $console.bind('keydown',function(event) {
-        if(event.keyCode == 13)
+        res = true;
+        switch(event.keyCode)
         {
-            cmd_text = get_cmd($(this));
-            execute_cmd(cmd_text);
-            // return false;
+            case 13://enter
+                cur_cmd = get_cmd($(this));
+                execute_cmd(cur_cmd);
+                history_cmd_before.push(cur_cmd);//执行命令后就将此命令放入之前历史中
+                up_down = false;
+                break;
+            case 38://上键
+                if(up_down == true)//如果之前使用了上下键才将命令推入history_cmd_after
+                {
+                    history_cmd_after.push(cur_cmd);
+                }
+                if(history_cmd_before.length > 0)//pop之前要判断是否有历史命令
+                {
+                    cur_cmd = history_cmd_before.pop();
+                    up_down = true;
+                    set_cmd($console,cur_cmd);
+                }
+                else//没有命令了就将up_down清为false，且清除之前显示的命令
+                {
+                    up_down = false;
+                    set_cmd($console,'');
+                }
+                res = false;
+                break;
+            case 40://下键
+                if(up_down == true)
+                {
+                    history_cmd_before.push(cur_cmd);
+                }
+                if(history_cmd_after.length > 0)
+                {
+                    cur_cmd = history_cmd_after.pop();
+                    up_down = true;
+                    set_cmd($console,cur_cmd);
+                }
+                else
+                {
+                    up_down = false;
+                    set_cmd($console,'');
+                }
+                res = false;
+                break;
+            default :
+                break;
         }
-        return true;
+        return res;
     });
     $('#back_color,#back_color_text').bind('change', function(event) {
         $console.css({'background-color': $(this).val()});
